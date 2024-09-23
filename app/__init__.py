c692c207c22469ee768d01a91afb001abb06866b
Manurxk <armanuramirez16@gmail.com>
2024-09-23 18:55:57 -0400

    feat: tabla pesona, index

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,49 @@
 from flask import Flask
 
 app = Flask(__name__)
+    
+
+
+
+
+
+  # Importar el blueprint de rutas principales
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Registrar los Blueprints
+    app.register_blueprint()
+
+    return app
+
+
+
+
+
 
 # importar referenciales
+
+from app.rutas.referenciales.index.index_routes import indmod
+
 from app.rutas.referenciales.ciudad.ciudad_routes import ciumod
 from app.rutas.referenciales.paises.pais_routes import paimod
 from app.rutas.referenciales.correo.correo_routes import cormod
 from app.rutas.referenciales.telefono.telefono_routes import telmod
 from app.rutas.referenciales.direccion.direccion_routes import dirmod
+from app.rutas.referenciales.persona.persona_routes import permod
 
 # registrar referenciales
 modulo0 = '/referenciales'
+
+app.register_blueprint(indmod, url_prefix=f'{modulo0}/index')
+
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
 app.register_blueprint(paimod, url_prefix=f'{modulo0}/paises')
 app.register_blueprint(cormod, url_prefix=f'{modulo0}/correo')
 app.register_blueprint(telmod, url_prefix=f'{modulo0}/telefono')
 app.register_blueprint(dirmod, url_prefix=f'{modulo0}/direccion')
+app.register_blueprint(permod, url_prefix=f'{modulo0}/persona')
 
 #ciudad
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
@@ -31,6 +59,10 @@ from app.rutas.referenciales.telefono.telefono_api import telapi
 
 #direccion
 from app.rutas.referenciales.direccion.direccion_api import dirapi
+
+#persona
+from app.rutas.referenciales.persona.persona_api import perapi
+
 
 # APIS v1
 #Ciudad
@@ -53,3 +85,7 @@ app.register_blueprint(telapi, url_prefix=version1)
 #direccion
 version1 = '/api/v1'
 app.register_blueprint(dirapi, url_prefix=version1)
+
+#persona
+version1 = '/api/v1'
+app.register_blueprint(perapi, url_prefix=version1)
---
+++ app/dao/referenciales/persona/PersonaDao.py
@@ -0,0 +1,128 @@
+from flask import current_app as app
+from app.conexion.Conexion import Conexion
+
+class PersonaDao:
+
+    def getPersonas(self):
+        personaSQL = """
+        SELECT id, nombre, apellido
+        FROM personas
+        """
+        conexion = Conexion()
+        con = conexion.getConexion()
+        cur = con.cursor()
+        try:
+            cur.execute(personaSQL)
+            personas = cur.fetchall()  # trae datos de la bd
+
+            # Transformar los datos en una lista de diccionarios
+            return [{'id': persona[0], 'nombre': persona[1], 'apellido': persona[2]} for persona in personas]
+
+        except Exception as e:
+            app.logger.error(f"Error al obtener todas las personas: {str(e)}")
+            return []
+
+        finally:
+            cur.close()
+            con.close()
+
+    def getPersonaById(self, id):
+        personaSQL = """
+        SELECT id, nombre, apellido
+        FROM personas WHERE id=%s
+        """
+        conexion = Conexion()
+        con = conexion.getConexion()
+        cur = con.cursor()
+        try:
+            cur.execute(personaSQL, (id,))
+            personaEncontrada = cur.fetchone()  # Obtener una sola fila
+            if personaEncontrada:
+                return {
+                    "id": personaEncontrada[0],
+                    "nombre": personaEncontrada[1],
+                    "apellido": personaEncontrada[2]
+                }  # Retornar los datos de la persona
+            else:
+                return None  # Retornar None si no se encuentra la persona
+        except Exception as e:
+            app.logger.error(f"Error al obtener persona: {str(e)}")
+            return None
+
+        finally:
+            cur.close()
+            con.close()
+
+    def guardarPersona(self, nombre, apellido):
+        insertPersonaSQL = """
+        INSERT INTO personas(nombre, apellido) VALUES(%s, %s) RETURNING id
+        """
+        conexion = Conexion()
+        con = conexion.getConexion()
+        cur = con.cursor()
+
+        try:
+            cur.execute(insertPersonaSQL, (nombre, apellido))
+            persona_id = cur.fetchone()[0]
+            con.commit()  # se confirma la inserción
+            return persona_id
+
+        except Exception as e:
+            app.logger.error(f"Error al insertar persona: {str(e)}")
+            con.rollback()  # retroceder si hubo error
+            return False
+
+        finally:
+            cur.close()
+            con.close()
+
+    def updatePersona(self, id, nombre, apellido):
+        updatePersonaSQL = """
+        UPDATE personas
+        SET nombre=%s, apellido=%s
+        WHERE id=%s
+        """
+        conexion = Conexion()
+        con = conexion.getConexion()
+        cur = con.cursor()
+
+        try:
+            cur.execute(updatePersonaSQL, (nombre, apellido, id,))
+            filas_afectadas = cur.rowcount  # Obtener el número de filas afectadas
+            con.commit()
+
+            return filas_afectadas > 0  # Retornar True si se actualizó al menos una fila
+
+        except Exception as e:
+            app.logger.error(f"Error al actualizar persona: {str(e)}")
+            con.rollback()
+            return False
+
+        finally:
+            cur.close()
+            con.close()
+
+    def deletePersona(self, id):
+        deletePersonaSQL = """
+        DELETE FROM personas
+        WHERE id=%s
+        """
+        conexion = Conexion()
+        con = conexion.getConexion()
+        cur = con.cursor()
+
+        try:
+            cur.execute(deletePersonaSQL, (id,))
+            rows_affected = cur.rowcount
+            con.commit()
+
+            return rows_affected > 0  # Retornar True si se eliminó al menos una fila
+
+        except Exception as e:
+            app.logger.error(f"Error al eliminar persona: {str(e)}")
+            con.rollback()
+            return False
+
+        finally:
+            cur.close()
+            con.close()
